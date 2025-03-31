87b327f273e4c1188d22db475e931e9a3f59a151
SsongCh94 <thddkwl94@gmail.com>
2025-03-31 14:51:14 +0900

    test : test

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
@@ -6,48 +17,32 @@ import { UpdateMovieDto } from './dto/update-movie.dto';
 @Controller('movie')
 @UseInterceptors(ClassSerializerInterceptor)
 export class MovieController {
-    constructor(private readonly movieService: MovieService) {}
+  constructor(private readonly movieService: MovieService) {}
 
-    @Get()
-    getMovies(
-        @Query('title') title?: string,
-    ){
-        return this.movieService.getManyMovies(
-            title
-        )
+  @Get()
+  getMovies(@Query('title test') title?: string) {
+    return this.movieService.getManyMovies(title);
+  }
+
+  @Get(':id')
+  getMovie(@Param('id') id: string) {
+    return this.movieService.getMovieById(+id);
+  }
+
+  @Post()
+  postMovie(@Body() body: CreateMovieDto) {
+    return this.movieService.createMovie(body);
+  }
+
+  @Patch(':id')
+  patchMovie(@Param('id') id: string, @Body() body: UpdateMovieDto) {
+    {
+      return this.movieService.updateMovie(+id, body);
     }
+  }
 
-    @Get(':id')
-    getMovie( @Param('id') id: string ){
-        return this.movieService.getMovieById(
-            +id
-        )
-    }
-
-    @Post()
-    postMovie(
-        @Body() body: CreateMovieDto,
-    ){
-        return this.movieService.createMovie(body);
-    }
-
-    @Patch(':id')
-    patchMovie(
-        @Param('id') id: string,
-        @Body() body: UpdateMovieDto,
-    ){{
-        return this.movieService.updateMovie(
-            +id,
-            body
-        );
-    }}
-
-    @Delete(':id')
-    deleteMovie(
-        @Param('id') id: string,
-    ){
-        return this.movieService.deleteMovie(
-            +id
-        );
-    }
+  @Delete(':id')
+  deleteMovie(@Param('id') id: string) {
+    return this.movieService.deleteMovie(+id);
+  }
 }
